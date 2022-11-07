c6fb7d6e53b0e8a521254e036edab2d4259d7415
702573N M4275CH1NK3 <59178587+702573N@users.noreply.github.com>
2022-11-07 18:59:36 +0100

    Completely revised statistic functionality

--- tasksCalendar/view.js
+++ tasksCalendar/view.js
@@ -28,13 +28,19 @@ var arrowRightIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height=
 var filterIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>';
 var monthIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><path d="M8 14h.01"></path><path d="M12 14h.01"></path><path d="M16 14h.01"></path><path d="M8 18h.01"></path><path d="M12 18h.01"></path><path d="M16 18h.01"></path></svg>';
 var weekIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><path d="M17 14h-6"></path><path d="M13 18H7"></path><path d="M7 14h.01"></path><path d="M17 18h.01"></path></svg>';
+
+var calendarClockIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5"></path><path d="M16 2v4"></path><path d="M8 2v4"></path><path d="M3 10h5"></path><path d="M17.5 17.5 16 16.25V14"></path><path d="M22 16a6 6 0 1 1-12 0 6 6 0 0 1 12 0Z"></path></svg>';
+var calendarCheckIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><path d="m9 16 2 2 4-4"></path></svg>';
+var calendarHeartIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 10V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h7"></path><path d="M16 2v4"></path><path d="M8 2v4"></path><path d="M3 10h18"></path><path d="M21.29 14.7a2.43 2.43 0 0 0-2.65-.52c-.3.12-.57.3-.8.53l-.34.34-.35-.34a2.43 2.43 0 0 0-2.65-.53c-.3.12-.56.3-.79.53-.95.94-1 2.53.2 3.74L17.5 22l3.6-3.55c1.2-1.21 1.14-2.8.19-3.74Z"></path></svg>';
+
 var cellTemplate = "<div class='cell {{class}}' data-weekday='{{weekday}}'><a class='internal-link cellName' href='{{dailyNote}}'>{{cellName}}</a><div class='cellContent'>{{cellContent}}</div></div>";
 var taskTemplate = "<a class='internal-link' href='{{taskPath}}'><div class='task {{class}}' style='{{style}}' title='{{title}}'><div class='inner'>{{taskContent}}</div></div></a>";
-const rootNode = dv.el("div", "", {cls: "tasksCalendar "+options, attr: {id: "tasksCalendar"+tid, view: view}});
+const rootNode = dv.el("div", "", {cls: "tasksCalendar "+options, attr: {id: "tasksCalendar"+tid, view: view, style: 'position:relative'}});
 
 // Initialze
 getMeta(tasks);
 setButtons();
+setStatisticPopUp();
 eval("get"+capitalize(view))(tasks, selectedDate);
 
 function getMeta(tasks) {
@@ -194,7 +200,7 @@ function setTaskContentContainer(currentDate) {
 };
 
 function setButtons() {
-	var buttons = "<button class='monthView' title='Month'>"+monthIcon+"</button><button class='weekView' title='Week'>"+weekIcon+"</button><button class='current'></button><button class='previous'>"+arrowLeftIcon+"</button><button class='next'>"+arrowRightIcon+"</button><button class='filter'>"+filterIcon+"</button>";
+	var buttons = "<button class='filter'>"+filterIcon+"</button><button class='monthView' title='Month'>"+monthIcon+"</button><button class='weekView' title='Week'>"+weekIcon+"</button><button class='current'></button><button class='previous'>"+arrowLeftIcon+"</button><button class='next'>"+arrowRightIcon+"</button><button class='statistic' percentage=''></button>";
 	rootNode.querySelector("span").appendChild(dv.el("div", buttons, {cls: "buttons", attr: {}}));
 	rootNode.querySelector("button."+view+"View").classList.add("active");
 	setButtonEvents();
@@ -207,36 +213,30 @@ function setButtonEvents() {
 			if (activeView == "month") {
 				selectedDate = moment(selectedDate).subtract(1, "months");
 				rootNode.querySelector(`#tasksCalendar${tid} .grid`).remove();
-				rootNode.querySelector(`#tasksCalendar${tid} .counter`).remove();
 				getMonth(tasks, selectedDate);
 			} else if (activeView == "week") {
 				selectedDate = moment(selectedDate).subtract(7, "days").startOf("week");
 				rootNode.querySelector(`#tasksCalendar${tid} .grid`).remove();
-				rootNode.querySelector(`#tasksCalendar${tid} .counter`).remove();
 				getWeek(tasks, selectedDate);
 			};
 		} else if ( btn.className == "current") {
 			if (activeView == "month") {
 				selectedDate = moment().date(1);
 				rootNode.querySelector(`#tasksCalendar${tid} .grid`).remove();
-				rootNode.querySelector(`#tasksCalendar${tid} .counter`).remove();
 				getMonth(tasks, selectedDate);
 			} else if (activeView == "week") {
 				selectedDate = moment().startOf("week");
 				rootNode.querySelector(`#tasksCalendar${tid} .grid`).remove();
-				rootNode.querySelector(`#tasksCalendar${tid} .counter`).remove();
 				getWeek(tasks, selectedDate);
 			};
 		} else if ( btn.className == "next" ) {
 			if (activeView == "month") {
 				selectedDate = moment(selectedDate).add(1, "months");
 				rootNode.querySelector(`#tasksCalendar${tid} .grid`).remove();
-				rootNode.querySelector(`#tasksCalendar${tid} .counter`).remove();
 				getMonth(tasks, selectedDate);
 			} else if (activeView == "week") {
 				selectedDate = moment(selectedDate).add(7, "days").startOf("week");
 				rootNode.querySelector(`#tasksCalendar${tid} .grid`).remove();
-				rootNode.querySelector(`#tasksCalendar${tid} .counter`).remove();
 				getWeek(tasks, selectedDate);
 			};
 		} else if ( btn.className == "filter" ) {
@@ -252,7 +252,6 @@ function setButtonEvents() {
 				selectedDate = moment(selectedDate).date(1);
 			};
 			rootNode.querySelector(`#tasksCalendar${tid} .grid`).remove();
-			rootNode.querySelector(`#tasksCalendar${tid} .counter`).remove();
 			getMonth(tasks, selectedDate);
 		} else if ( btn.className == "weekView" ) {
 			rootNode.querySelector("button.active").classList.remove("active");
@@ -263,9 +262,10 @@ function setButtonEvents() {
 				selectedDate = moment().startOf("week");
 			};
 			rootNode.querySelector(`#tasksCalendar${tid} .grid`).remove();
-			rootNode.querySelector(`#tasksCalendar${tid} .counter`).remove();
 			getWeek(tasks, selectedDate);
-		};
+		} else if ( btn.className == "statistic" ) {
+			rootNode.querySelector(".statisticPopup").classList.toggle("active");
+		}
 		btn.blur();
 	})));
 };
@@ -282,13 +282,43 @@ function setWrapperEvents() {
 	})));
 };
 
-function setCounter(dueCounter, doneCounter) {
-	var percentage = Math.round(100/(dueCounter+doneCounter)*doneCounter);
+function setStatisticPopUp() {
+	var statistic = "<li id='statisticDone'></li>";
+	statistic += "<li id='statisticDue'></li>";
+	statistic += "<li id='statisticOverdue'></li>";
+	statistic += "<li class='break'></li>";
+	statistic += "<li id='statisticStart'></li>";
+	statistic += "<li id='statisticScheduled'></li>";
+	statistic += "<li id='statisticRecurrence'></li>";
+	statistic += "<li class='break'></li>";
+	statistic += "<li id='statisticDailyNote'></li>";
+	rootNode.querySelector("span").appendChild(dv.el("ul", statistic, {cls: "statisticPopup"}));
+};
+
+function setStatisticValues(dueCounter, doneCounter, overdueCounter, startCounter, scheduledCounter, recurrenceCounter, dailyNoteCounter) {
+	var taskCounter = parseInt(dueCounter+doneCounter+overdueCounter);
+	var tasksRemaining = taskCounter - doneCounter;
+	var percentage = Math.round(100/(dueCounter+doneCounter+overdueCounter)*doneCounter);
 	percentage = isNaN(percentage) ? 100 : percentage;
-	var progress = "<div class='progress' value='"+percentage+"'></div> " + percentage + "% | " + doneCounter + "/" + parseInt(dueCounter+doneCounter) + " Tasks Completed";
-	var counter = progress + " | Due: " + dueCounter + " | Done: " + doneCounter;
-	rootNode.querySelector("span").appendChild(dv.el("div", counter, {cls: "counter"}));
-}
+	
+	if (dueCounter == 0 && doneCounter == 0) {
+		rootNode.querySelector("button.statistic").innerHTML = calendarHeartIcon;
+	} else if (tasksRemaining > 0) {
+		rootNode.querySelector("button.statistic").innerHTML = calendarClockIcon;
+	} else if (dueCounter == 0 && doneCounter != 0) {
+		rootNode.querySelector("button.statistic").innerHTML = calendarCheckIcon;
+	};
+	if (tasksRemaining > 99) {tasksRemaining = "⚠️"};
+	rootNode.querySelector("button.statistic").setAttribute("data-percentage", percentage);
+	rootNode.querySelector("button.statistic").setAttribute("data-remaining", tasksRemaining);
+	rootNode.querySelector("#statisticDone").innerText = "✅ Done: " + doneCounter + "/" + taskCounter;
+	rootNode.querySelector("#statisticDue").innerText = "📅 Due: " + dueCounter;
+	rootNode.querySelector("#statisticOverdue").innerText = "⚠️ Overdue: " + overdueCounter;
+	rootNode.querySelector("#statisticStart").innerText = "🛫 Start: " + startCounter;
+	rootNode.querySelector("#statisticScheduled").innerText = "⏳ Scheduled: " + scheduledCounter;
+	rootNode.querySelector("#statisticRecurrence").innerText = "🔁 Recurrence: " + recurrenceCounter;
+	rootNode.querySelector("#statisticDailyNote").innerText = "📄 Daily Notes: " + dailyNoteCounter;
+};
 
 function getMonth(tasks, month) {
 	var currentTitle = "<span>"+moment(month).format("MMMM")+"</span><span> "+moment(month).format("YYYY");
@@ -299,6 +329,11 @@ function getMonth(tasks, month) {
 	var lastDateOfMonth = moment(month).endOf("month").format("D");
 	var dueCounter = 0;
 	var doneCounter = 0;
+	var overdueCounter = 0;
+	var startCounter = 0;
+	var scheduledCounter = 0;
+	var recurrenceCounter = 0;
+	var dailyNoteCounter = 0;
 	
 	// Move First Week Of Month To Second Week In Month View
 	if (firstDayOfMonth == 0) { firstDayOfMonth = 7};
@@ -314,23 +349,22 @@ function getMonth(tasks, month) {
 			gridHeads += "<div class='gridHead' data-weekday='" + weekDayNr + "'>" + weekDayName + "</div>";
 		};
 	};
-	gridContent += "<div class='gridHeads'><div class='gridHead'></div>"+gridHeads+"</div>";
 	
 	// Set Wrappers
 	var wrappers = "";
-	var start = 0-firstDayOfMonth+firstDayOfWeek;
+	var starts = 0-firstDayOfMonth+firstDayOfWeek;
 	for (w=1; w<7; w++) {
 		var wrapper = "";
 		var weekNr = "";
 		var yearNr = "";
 		var monthName = moment(month).format("MMM").replace(".","").substring(0,3);
-		for (i=start;i<start+7;i++) {
-			if (i==start) {
+		for (i=starts;i<starts+7;i++) {
+			if (i==starts) {
 				weekNr = moment(month).add(i, "days").format("w");
 				yearNr = moment(month).add(i, "days").format("YYYY");
 			};
 			var currentDate = moment(month).add(i, "days").format("YYYY-MM-DD");
-			dailyNoteFolder != null ? dailyNoteFolder+"/"+currentDate : currentDate;
+			if (!dailyNoteFolder) {var dailyNotePath = currentDate} else {var dailyNotePath = dailyNoteFolder+"/"+currentDate};
 			var weekDay = moment(month).add(i, "days").format("d");
 			var shortDayName = moment(month).add(i, "days").format("D");
 			var longDayName = moment(month).add(i, "days").format("D. MMM");
@@ -346,17 +380,25 @@ function getMonth(tasks, month) {
 				dueCounter += scheduled.length;
 				dueCounter += dailyNote.length;
 				doneCounter += done.length;
+				startCounter += start.length;
+				scheduledCounter += scheduled.length;
+				recurrenceCounter += recurrence.length;
+				dailyNoteCounter += dailyNote.length;
+				// Get Overdue Count From Today
+				if (moment().format("YYYY-MM-DD") == moment(month).add(i, "days").format("YYYY-MM-DD")) {
+					overdueCounter = overdue.length;
+				};
 			};
-
+			
 			// Set New Content Container
 			var cellContent = setTaskContentContainer(currentDate);
 		
 			// Set Cell Name And Weekday
 			if ( moment(month).add(i, "days").format("D") == 1 ) {
-				var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", longDayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNoteFolder);
+				var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", longDayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNotePath);
 				cell = cell.replace("{{class}}", "{{class}} newMonth");
 			} else {
-				var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", shortDayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNoteFolder);
+				var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", shortDayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNotePath);
 			};
 		
 			// Set prevMonth, currentMonth, nextMonth
@@ -372,12 +414,13 @@ function getMonth(tasks, month) {
 			wrapper += cell;
 		};
 		wrappers += "<div class='wrapper'><div class='wrapperButton' data-week='"+weekNr+"' data-year='"+yearNr+"'>W"+weekNr+"</div>"+wrapper+"</div>";
-		start += 7;
+		starts += 7;
 	};
+	gridContent += "<div class='gridHeads'><div class='gridHead'></div>"+gridHeads+"</div>";
 	gridContent += "<div class='wrappers' data-month='"+monthName+"'>"+wrappers+"</div>";
 	rootNode.querySelector("span").appendChild(dv.el("div", gridContent, {cls: "grid", attr:{'data-view': "month"}}));
 	setWrapperEvents();
-	setCounter(dueCounter, doneCounter);
+	setStatisticValues(dueCounter, doneCounter, overdueCounter, startCounter, scheduledCounter, recurrenceCounter, dailyNoteCounter);
 };
 
 function getWeek(tasks, week) {
@@ -388,10 +431,15 @@ function getWeek(tasks, week) {
 	var weekNr = moment(week).format("[W]w");
 	var dueCounter = 0;
 	var doneCounter = 0;
+	var overdueCounter = 0;
+	var startCounter = 0;
+	var scheduledCounter = 0;
+	var recurrenceCounter = 0;
+	var dailyNoteCounter = 0;
 	
 	for (i=0-currentWeekday+firstDayOfWeek;i<7-currentWeekday+firstDayOfWeek;i++) {
 		var currentDate = moment(week).add(i, "days").format("YYYY-MM-DD");
-		dailyNoteFolder != null ? dailyNoteFolder+"/"+currentDate : currentDate;
+		if (!dailyNoteFolder) {var dailyNotePath = currentDate} else {var dailyNotePath = dailyNoteFolder+"/"+currentDate};
 		var weekDay = moment(week).add(i, "days").format("d");
 		var dayName = moment(currentDate).format("ddd D.");
 		var longDayName = moment(currentDate).format("ddd, D. MMM");
@@ -405,18 +453,25 @@ function getWeek(tasks, week) {
 		dueCounter += scheduled.length;
 		dueCounter += dailyNote.length;
 		doneCounter += done.length;
+		startCounter += start.length;
+		scheduledCounter += scheduled.length;
+		recurrenceCounter += recurrence.length;
+		dailyNoteCounter += dailyNote.length;
+		if (moment().format("YYYY-MM-DD") == moment(week).add(i, "days").format("YYYY-MM-DD")) {
+			overdueCounter = overdue.length;
+		};
 	
 		// Set New Content Container
 		var cellContent = setTaskContentContainer(currentDate);
 		
 		// Set Cell Name And Weekday
-		var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", longDayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNoteFolder);
+		var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", longDayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNotePath);
 
 		// Set Cell Name And Weekday
 		if ( moment(week).add(i, "days").format("D") == 1 ) {
-			var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", longDayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNoteFolder);
+			var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", longDayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNotePath);
 		} else {
-			var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", dayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNoteFolder);
+			var cell = cellTemplate.replace("{{date}}", currentDate).replace("{{cellName}}", dayName).replace("{{cellContent}}", cellContent).replace("{{weekday}}", weekDay).replace("{{dailyNote}}", dailyNotePath);
 		};
 			
 		// Set Today, Before Today, After Today
@@ -430,5 +485,5 @@ function getWeek(tasks, week) {
 		gridContent += cell;
 	};
 	rootNode.querySelector("span").appendChild(dv.el("div", gridContent, {cls: "grid", attr:{'data-view': "week", 'data-week': weekNr}}));
-	setCounter(dueCounter, doneCounter);
+	setStatisticValues(dueCounter, doneCounter, overdueCounter, startCounter, scheduledCounter, recurrenceCounter, dailyNoteCounter);
 };
