8c83cca21538c006fa0cdc2af08ac0bc0b9c1e5c
702573N M4275CH1NK3 <59178587+702573N@users.noreply.github.com>
2022-11-19 09:45:49 +0100

    Opt: Filter, Fix: Doubled if statements
    ### Some fixes
    - Fix: Remove doubled if statement for filter button event
    - Fix: Style of cancelled tasks to looks less prominent
    - Fix: Disable focusing done tasks if filter is active
    
    ### What's new?
    - Add: Active done-filter now hides completed and cancelled tasks
    - Add: New `noLineClamp` option to show full task description text without line clamp
    - Add: Task hover styles on desktop only

--- tasksCalendar/view.js
+++ tasksCalendar/view.js
@@ -2,11 +2,19 @@ let {pages, view, firstDayOfWeek, globalTaskFilter, dailyNoteFolder, startPositi
 
 // Get, Set, Eval Pages
 if (pages=="") {
+	console.log("1")
 	var tasks = dv.pages().file.tasks;
 } else {
+	console.log("2")
 	if (pages.startsWith("dv.pages")) {
+		console.log("3")
+		console.log(pages);
+		
 		var tasks = eval(pages);
+		
+		console.log(tasks);
 	} else {
+		console.log("4")
 		var tasks = dv.pages(pages).file.tasks;
 	};
 };
@@ -28,7 +36,6 @@ var arrowRightIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height=
 var filterIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>';
 var monthIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><path d="M8 14h.01"></path><path d="M12 14h.01"></path><path d="M16 14h.01"></path><path d="M8 18h.01"></path><path d="M12 18h.01"></path><path d="M16 18h.01"></path></svg>';
 var weekIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><path d="M17 14h-6"></path><path d="M13 18H7"></path><path d="M7 14h.01"></path><path d="M17 18h.01"></path></svg>';
-
 var calendarClockIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5"></path><path d="M16 2v4"></path><path d="M8 2v4"></path><path d="M3 10h5"></path><path d="M17.5 17.5 16 16.25V14"></path><path d="M22 16a6 6 0 1 1-12 0 6 6 0 0 1 12 0Z"></path></svg>';
 var calendarCheckIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><path d="m9 16 2 2 4-4"></path></svg>';
 var calendarHeartIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 10V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h7"></path><path d="M16 2v4"></path><path d="M8 2v4"></path><path d="M3 10h18"></path><path d="M21.29 14.7a2.43 2.43 0 0 0-2.65-.52c-.3.12-.57.3-.8.53l-.34.34-.35-.34a2.43 2.43 0 0 0-2.65-.53c-.3.12-.56.3-.79.53-.95.94-1 2.53.2 3.74L17.5 22l3.6-3.55c1.2-1.21 1.14-2.8.19-3.74Z"></path></svg>';
@@ -239,8 +246,9 @@ function setButtonEvents() {
 			};
 		} else if ( btn.className == "filter" ) {
 			rootNode.classList.toggle("noDone");
-		} else if ( btn.className == "filter" ) {
-			rootNode.classList.toggle("noDone");
+			rootNode.classList.toggle("noCancelled");
+			rootNode.querySelector('#statisticDone').classList.remove("active");
+			rootNode.classList.remove("focusDone");
 		} else if ( btn.className == "monthView" ) {
 			rootNode.querySelector("button.active").classList.remove("active");
 			btn.classList.add("active");
@@ -506,4 +514,4 @@ function getWeek(tasks, week) {
 	};
 	rootNode.querySelector("span").appendChild(dv.el("div", gridContent, {cls: "grid", attr:{'data-view': "week", 'data-week': weekNr}}));
 	setStatisticValues(dueCounter, doneCounter, overdueCounter, startCounter, scheduledCounter, recurrenceCounter, dailyNoteCounter);
-};
+	};
